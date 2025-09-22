ca779d0c023ccb29e64f18b37f00b1556abf2c5f
ryuhm8-creator <ryuhm8@kookmin.ac.kr>
2025-09-22 19:14:30 +0900

    05_practice_2.ino

---
+++ 05_practice_2.ino
@@ -0,0 +1,26 @@
+void setup() {
+pinMode(7, OUTPUT); // 7번 핀을 출력으로 설정
+
+}
+void loop() {
+digitalWrite(7, HIGH); // LED 켜기
+delay(1000);           // 1초 대기
+digitalWrite(7, LOW);  // LED 끄기
+delay(1000);           // 1초 대기
+
+int count = 0;
+while(count<5) {
+  digitalWrite(7, HIGH); // LED 켜기
+  delay(100);           // 1초 대기
+  digitalWrite(7, LOW);  // LED 끄기
+  delay(100); // 1초 대기
+  count++;
+
+}
+void loop() {
+  digitalWrite(7, LOW); 
+  while(true);
+}
+}
+
+# 
